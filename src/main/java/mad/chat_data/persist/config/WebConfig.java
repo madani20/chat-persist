a68f69ff845ccf7f7e7595279d5df1a090fff17c
Madani <khouane.madani@free.fr>
2025-05-20 16:27:18 +0200

    Modifié les sources autorisées

--- src/main/java/mad/chat_data/persist/config/WebConfig.java
+++ src/main/java/mad/chat_data/persist/config/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 //.allowedOrigins("http://192.168.1.179:4200", "http://192.168.1.124:4200", "http://localhost:4200")
-                //.allowedOrigins("https://chat-auth-j6ww.onrender.com/**", "https://chat-client-e2lv.onrender.com/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://chat-auth-j6ww.onrender.com/**", "https://chat-client-e2lv.onrender.com/**")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
